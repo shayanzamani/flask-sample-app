35ff3fc4d3cbaff96edc3cb78a59de61684c464f
Shayan Zamani <shayan.zamani1@ucalgary.ca>
2024-02-05 22:12:36 -0700

    Adding about me

--- app/app.py
+++ app/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
 	return "Hello World!"
 
+@app.route('/about/')
+def about_me():
+	return "This is developed by me!"
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
